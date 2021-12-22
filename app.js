203a19b2629a3c43ce973b23cd28c52fecf3a5e5
yusufakbs <yusufakbas47@hotmail.com>
2021-12-22 04:09:38 +0300

    Static Files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-  console.log('function Working');
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+// MIDDLEWARES
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
